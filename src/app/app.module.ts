7ce6aea80bd994d37db53dde7adf1628ef2a4ec7
santiago-portugal <santiportugal15@gmail.com>
2020-09-23 12:22:55 -0500

    todo va bien

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
+import { MatToolbarModule} from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatMenuModule} from '@angular/material/menu';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { QuienesSomosComponent } from './components/quienes-somos/quienes-somos.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { ContactenosComponent } from './components/contactenos/contactenos.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    QuienesSomosComponent,
+    ProductosComponent,
+    ServiciosComponent,
+    ContactenosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatGridListModule,
+    MatCardModule,
+    MatTabsModule,
+    MatMenuModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/inicio/inicio.component.ts
@@ -0,0 +1,94 @@
+import { Component, OnInit } from '@angular/core';
+// import {} from '@angular/material'
+ 
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css']
+})
+export class InicioComponent implements OnInit {
+  filas = [
+    {text: 'One', pepito: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', pepito: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', pepito: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', pepito: 2, rows: 1, color: '#DDBDF1'},
+  ];
+  public Status=0;
+  public StatusProduct=0;
+
+  constructor() { }
+
+  
+  ngOnInit(): void {
+  }
+
+  ChangeSecond(){
+    console.log('Este es el status', this.Status)
+    this.Status=2;
+    console.log('Este es el status luego del cambio', this.Status)
+  }
+  ChangeZero(){
+    console.log('Este es el status', this.Status)
+    this.Status=0;
+    console.log('Este es el status luego del cambio', this.Status)
+  }
+  ChangeTercero(){
+    console.log('Este es el status', this.Status)
+    this.Status=3;
+    console.log('Este es el status luego del cambio', this.Status)
+  }
+  ChangeCuarto(){
+    console.log('Este es el status', this.Status)
+    this.Status=4;
+    console.log('Este es el status luego del cambio', this.Status)
+  }
+  ChangeQuinto(){
+    console.log('Este es el status', this.Status)
+    this.Status=5;
+    console.log('Este es el status luego del cambio', this.Status)
+  }
+
+  ChangeP1(){
+    console.log('Este es el status', this.StatusProduct)
+    this.Status=3;
+    this.StatusProduct=6;
+    console.log('Este es el status luego del cambio', this.StatusProduct)
+  }
+  ChangeP2(){
+    console.log('Este es el status', this.StatusProduct)
+    this.Status=3;
+    this.StatusProduct=7;
+    console.log('Este es el status luego del cambio', this.StatusProduct)
+  }
+  ChangeP3(){
+    console.log('Este es el status', this.StatusProduct)
+    this.Status=3;
+    this.StatusProduct=8;
+    console.log('Este es el status luego del cambio', this.StatusProduct)
+  }
+  ChangeP4(){
+    console.log('Este es el status', this.StatusProduct)
+    this.Status=3;
+    this.StatusProduct=9;
+    console.log('Este es el status luego del cambio', this.StatusProduct)
+  }
+  ChangeP5(){
+    console.log('Este es el status', this.StatusProduct)
+    this.Status=3;
+    this.StatusProduct=10;
+    console.log('Este es el status luego del cambio', this.StatusProduct)
+  }
+  ChangeP6(){
+    console.log('Este es el status', this.StatusProduct)
+    this.Status=3;
+    this.StatusProduct=11;
+    console.log('Este es el status luego del cambio', this.StatusProduct)
+  }
+  ChangeP7(){
+    console.log('Este es el status', this.StatusProduct)
+    this.Status=3;
+    this.StatusProduct=12;
+    console.log('Este es el status luego del cambio', this.StatusProduct)
+  }
+ 
+}
