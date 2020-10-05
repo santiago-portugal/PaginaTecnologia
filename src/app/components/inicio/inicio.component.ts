27cf1a752aedcca9d75433fbd3f16e2c3ffa41a1
Santiago portugal <santiportugal15@gmail.com>
2020-10-05 11:52:46 -0500

    listo imagenes

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: ['./inicio.component.css',]
 })
 export class InicioComponent implements OnInit {
   filas = [
